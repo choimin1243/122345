e52e8c0dd27ca1b8ba47ab6e77d1416ddd12b1fc
Litchy_is_He <72008587+lithu-lithish@users.noreply.github.com>
2023-04-19 17:51:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,24 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://oaliyidm:dIPyvR3MxTFPOOn1qvp4hQgoLLjOxJXQ@snuffleupagus.db.elephantsql.com/oaliyidm"
 
-SQLALCHEMY_DATABASE_URL =   "postgresql://todo_authenticaion_user:PqD3zGFFSESOR9Fump9TJgTeVuVVwZ1c@dpg-cgvntequt4mc4k9i5fsg-a/todo_authenticaion"
-
-# MYSQL Series
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
 
-# MYSQL Series
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL
-# )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
