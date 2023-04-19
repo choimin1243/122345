b7e4477a344858427925dd97e1c047d90746c8e6
Litchy_is_He <72008587+lithu-lithish@users.noreply.github.com>
2023-04-19 17:11:50 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://todo_authenticaion_user:PqD3zGFFSESOR9Fump9TJgTeVuVVwZ1c@dpg-cgvntequt4mc4k9i5fsg-a.oregon-postgres.render.com/todo_authenticaion"
 
+SQLALCHEMY_DATABASE_URL =   "postgresql://todo_authenticaion_user:PqD3zGFFSESOR9Fump9TJgTeVuVVwZ1c@dpg-cgvntequt4mc4k9i5fsg-a/todo_authenticaion"
 
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
