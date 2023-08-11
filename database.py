2b01a965bce22a7ed0ac5b51cebe69aaaa9f863e
yeonsama <62858055+choimin1243@users.noreply.github.com>
2023-08-11 21:05:23 +0900

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://oaliyidm:dIPyvR3MxTFPOOn1qvp4hQgoLLjOxJXQ@snuffleupagus.db.elephantsql.com/oaliyidm"
+SQLALCHEMY_DATABASE_URL = "postgresql://qhceykvr:VcmJgJKWHEw9QMxmdjRRK_zUBmU97_GD@satao.db.elephantsql.com/qhceykvr"
 
 
 engine = create_engine(
